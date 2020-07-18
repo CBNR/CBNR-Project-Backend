3fca89f5df4f550f87d4ee3c7036b633f670d766
bryan.varic@gmail.com <B-Gunawan>
2020-07-18 22:51:17 +1000

    Split connection and rcvChat callbacks
    + Now we can add callbacks individually in their own methods.

--- src/chatServer.ts
+++ src/chatServer.ts
@@ -16,6 +16,10 @@ class ChatServer{
     private server : http.Server;
     private io : socketIO.Server;
 
+    /**
+     * Constructor
+     * @param port connection port number. usually 3001.
+     */
     constructor (port : number){
         this.port = port;
         this.app = express();
@@ -32,27 +36,34 @@ class ChatServer{
         })
     }
 
+    /**
+     * Declare initial connection callback
+     */
     private initCallbacks(){
         this.io.on('connection', (socket) => {
-            console.log('[New Connection] ' + socket.id);
-            socket.on('chat_msg', (chatMessage : ChatMessage) => {
-                this.io.sockets.emit('chat_msg', chatMessage);
-            });
+            this.connectionCB(socket);
         });
     }
-    
-    // private initCallbacks(){
-    //     this.io.on('connection', (socket) => {
-    //         console.log('new connection');
-    //     });
-    // }
 
-    // private connectionCB(socket : socketIO.Socket){
-    // }
+    /**
+     * Connection callback. This method initialises all socket callbacks.
+     * @param socket 
+     */
+    private connectionCB(socket : socketIO.Socket){
+        console.log('[New Connection] ' + socket.id);
+        socket.on('chat_msg', (ChatMessage) => {
+            this.rcvChatCB(ChatMessage);
+        })
+    }
 
-    // private rcvChatCB(chatMessage : ChatMessage) : void{
-    // }
-
+    /**
+     * Chat receive callback. When a chat is received, emit the same signal
+     * all other sockets.
+     * @param chatMessage 
+     */
+    private rcvChatCB(chatMessage : ChatMessage) : void{
+        this.io.sockets.emit('chat_msg', chatMessage);
+    }
 }
 
 let chatServer = new ChatServer(3001);
