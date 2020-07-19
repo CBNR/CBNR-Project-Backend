282e340180578da949a332b1159d6f9b70a7b607
B-Gunawan <bryan.varic@gmail.com>
2020-07-19 22:36:14 +1000

    Added split chatroom functionality
    + New class called chatroom. each object represents a chatroom.
    + TODO: Implement logging system.

---
+++ src/chatRoom.ts
@@ -0,0 +1,61 @@
+
+import { User, ChatMessage } from './chatServer';
+
+export class ChatRoom {
+
+    private users : User[];
+    public id : string;
+    public population : number = 0;
+    // TODO: Implement human readable room name
+
+    constructor(roomId : string){
+        this.users = [];
+        this.id = roomId
+        console.log("New room created");
+    }
+
+    public addUser(user : User){
+        // Add user to room's user list
+        this.users.push(user);
+        // Announce to everyone in room
+        let msg : ChatMessage = {
+            handle : 'Server',
+            message : user.name + " has entered the room"
+        }
+        // Register room reference on user
+        user.room = this;
+        this.broadcastMessage(msg);
+        this.population++;
+    }
+
+    public removeUser(user : User){
+        // Remove user from room's user list
+        const index = this.users.indexOf(user, 0);
+        if (index > -1){
+            this.users.splice(index, 1);
+        }
+        // Announce to everyone in room
+        let msg : ChatMessage = {
+            handle : 'Server',
+            message : user.name + " has left the room"
+        }
+        this.broadcastMessage(msg);
+        user.room = undefined;
+        this.population--;
+    }
+
+    public broadcastMessage(msg : ChatMessage){
+        this.users.forEach(user => {
+            user.socket.emit('chat_msg', msg);
+        });
+    }
+
+    public broadcastJoin(user : User){
+        this.users.forEach(user => {
+            user.socket.emit('user_join');
+        });
+    }
+
+}
+
+
--- src/chatServer.ts
+++ src/chatServer.ts
@@ -1,94 +1,154 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
-import socketIO from 'socket.io';
+import socketIO, { Socket, listen, Room } from 'socket.io';
+import {ChatRoom} from './chatRoom';
+import md5 from 'md5';
 
-interface ChatMessage{
+export interface ChatMessage{
     handle : string,
     message: string,
-    target?: string // TODO: private chat targets to be implemented
-}
+    target?: string, // TODO: private chat targets to be implemented
+    auth? : Object
+};
+
+export interface User{
+    id  : string,
+    name : string,
+    socket : socketIO.Socket,
+    room? : ChatRoom
+};
 
 class ChatServer{
     
-    private port : number;
-    private app : express.Application;
-    private server : http.Server;
-    private io : socketIO.Server;
+    private readonly ROOM_LIMIT : number = 10;
 
-    /**
-     * Constructor
-     * @param port connection port number. usually 3001.
-     */
-    constructor (port : number){
+    private app     : express.Application = express();
+    private port    : number;
+    private server  : http.Server;
+    private io      : socketIO.Server;
+
+    private rooms   : Map<string, ChatRoom> = new Map<string, ChatRoom>();
+    private users   : Map<string, User> = new Map<string, User>();
+
+    constructor(port : number){
+        // Start http server
         this.port = port;
-        this.app = express();
-        this.server = this.app.listen(port, () => {
+        this.server = this.app.listen(this.port, ()=>{
             console.log('Server listening on port ' + this.port);
         })
+        // Bind socket server to http server
         this.io = socketIO(this.server);
         this.initCallbacks();
-        // DEBUG
+
+        // returns list of room IDs
+        this.app.get('/roomlist', (req,res) => {
+            let roomlist = Array.from(this.rooms.keys());
+            res.type('json');
+            res.json(roomlist);
+        })
+
+        // Backend debug only
         // this.app.get('/', (req,res) => {
         //     res.sendFile(path.join(__dirname, '..', 'debug', 'index.html')) //TODO: change to be compatible with front end.
         // })
     }
 
-    /**
-     * Declare initial connection callback
-     */
     private initCallbacks(){
         this.io.on('connection', (socket) => {
-            this.connectionCB(socket);
+            console.log('[New Connection] SID : ' + socket.id);
+            // TODO: Implement authentication. Replace this bit
+            
+            this.users.set(socket.id, {
+                id : 'U' + socket.id,
+                name : 'Name#' + socket.id,
+                socket : socket
+            });
+            let user = this.users.get(socket.id) as User;
+
+            // Declare default event callbacks
+            socket.on('disconnect', ()=>            {this.disconnectCB(socket.id)});
+
+            // Declare chatroom event callbacks
+            socket.on('chat_msg',   (chatMessage)=> {this.chatMsgCB(user, chatMessage);});
+            socket.on('join_room',  (chatRoom)=>    {this.joinRoomCB(user, chatRoom);});
+            socket.on('create_room',()=>            {this.createRoomCB(user);});
+            socket.on('leave_room', ()=>            {this.leaveRoomCB(user);});
         });
     }
 
-    /**
-     * Connection callback. This method initialises all socket callbacks.
-     * @param socket 
-     */
-    private connectionCB(socket : socketIO.Socket){
-        console.log('[New Connection] ' + socket.id);
-        
-        // Handle custom events
-        socket.on('chat_msg', (chatMessage) => {
-            this.rcvChatCB(chatMessage);
-        });
-        
-        // Handle builtin events
-        socket.on('disconnect', () => {
-            this.disconnectCB(socket);
-        });
-        socket.on('connect_timeout', () => {
-            this.timeoutCB(socket);
-        });
+    private disconnectCB(socketid : string){
+        let user = this.users.get(socketid);
+        if (user != undefined){
+            if (user.room != undefined){
+                this.leaveRoomCB(user);
+            }
+            this.users.delete(socketid);
+            console.log(user.id + ' disconnected');
+        } else {
+            // TODO : error handler here
+        }
     }
 
-    /**
-     * Chat receive callback. When a chat is received, emit the same signal
-     * all other sockets.
-     * @param chatMessage 
-     */
-    private rcvChatCB(chatMessage : ChatMessage) : void{
-        this.io.sockets.emit('chat_msg', chatMessage);
+    private chatMsgCB(user : User, chatMessage : ChatMessage){
+        if (user.room != undefined){
+            user.room.broadcastMessage(chatMessage);
+            user.socket.emit('chat_msg_c', true);
+        } else {
+            user.socket.emit('chat_msg_c', false);
+        }
     }
 
-    /**
-     * Disconnect callback
-     * @param socket 
-     */
-    private disconnectCB(socket : socketIO.Socket){
-        console.log('[Connection closed] ' + socket.id);
+    private joinRoomCB(user : User, roomId : string){
+        let room = this.rooms.get(roomId);
+        if (room != undefined){
+            // Check if user is in another room. remove from room if true.
+            if (user.room != undefined){
+                this.leaveRoomCB(user);
+            }
+            // Join Room
+            user.socket.emit('join_room', true);
+            // TODO : implement room population limit.
+            room.addUser(user);
+            console.log(user.id +' user joined room ' + roomId);
+        } else {
+            user.socket.emit('join_room', false);
+            // TODO: error handler here
+            console.log(user.id +' attemped to join an empty room #' + roomId);
+        }
+    }
+    
+    private createRoomCB(user : User){
+        if (this.rooms.size < this.ROOM_LIMIT){
+            let roomId = md5(user.socket.id).substr(0,10);
+            this.rooms.set(roomId, new ChatRoom(roomId));
+            this.joinRoomCB(user, roomId);
+            user.socket.emit('create_room', true);
+            this.broadcastRoomListUpdate();
+        } else {
+            user.socket.emit('create_room', false);
+            // TODO: error handler here
+        }
     }
 
-    /**
-     * Connection timeout callback.
-     * @param socket 
-     */
-    private timeoutCB(socket : socketIO.Socket){
-        console.log('[Timeout] ' + socket.id);
+    private leaveRoomCB(user : User){
+        let room : ChatRoom | undefined = user.room;
+        if (room != undefined){
+            room.removeUser(user);
+            user.socket.emit('leave_room', true);
+            if (room.population == 0) {
+                this.rooms.delete(room.id);
+                this.broadcastRoomListUpdate();
+            }
+        } else {
+            user.socket.emit('leave_room', false);
+            //TODO: error handler here
+        }
     }
 
+    private broadcastRoomListUpdate(){
+        this.io.sockets.emit('roomlist_update');
+    }
 }
 
 let chatServer = new ChatServer(3001);
